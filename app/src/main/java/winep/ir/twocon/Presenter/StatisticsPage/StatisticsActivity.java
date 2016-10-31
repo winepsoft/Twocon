e16a2a30ae3cccfb1aadd7c1dc92cfc8c79e977e
shaisteS <shs.woem@gmail.com>
2016-10-31 17:56:35 +0330

    set font size,text color and data in statistics page

--- app/src/main/java/winep/ir/twocon/Presenter/StatisticsPage/StatisticsActivity.java
+++ app/src/main/java/winep/ir/twocon/Presenter/StatisticsPage/StatisticsActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
@@ -34,6 +35,12 @@ public class StatisticsActivity extends AppCompatActivity {
     private PieChart chart2;
     private PieChart chart3;
     private String groupTitle;
+    private TextView txtLeftTop;
+    private TextView txtLeftBottom;
+    private TextView txtRightTop;
+    private TextView txtRightBottom;
+    private Context context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,66 +50,74 @@ public class StatisticsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         forceRTLIfSupported();
+        context=this;
 
         chart1=(LineChart)findViewById(R.id.chart1);
         chart2=(PieChart)findViewById(R.id.chart2);
         chart3=(PieChart)findViewById(R.id.chart3);
+        txtLeftTop=(TextView)findViewById(R.id.txt_value_left_top);
+        Utilities.getInstance().setFontTextView(context,txtLeftTop);
+        txtLeftTop.setText("95/07/07");
+
+        txtLeftBottom=(TextView)findViewById(R.id.txt_value_left_bottom);
+        Utilities.getInstance().setFontTextView(context,txtLeftBottom);
+        txtLeftBottom.setText("95/08/10");
+
+        txtRightTop=(TextView)findViewById(R.id.txt_value_right_top);
+        Utilities.getInstance().setFontTextView(context,txtRightTop);
+        txtRightTop.setText("200");
+
+        txtRightBottom=(TextView)findViewById(R.id.txt_value_right_bottom);
+        Utilities.getInstance().setFontTextView(context,txtRightBottom);
+        txtRightBottom.setText("1000");
+
+
 
         //LineChart Set Data
         ArrayList<String> xLine=new ArrayList<>();
-        xLine.add("0");
-        xLine.add("1");
-        xLine.add("2");
-        xLine.add("3");
-        xLine.add("4");
-        xLine.add("5");
-        xLine.add("6");
-        xLine.add("7");
-        xLine.add("8");
-        xLine.add("9");
-        xLine.add("10");
-        xLine.add("11");
-        xLine.add("12");
-        xLine.add("13");
-        xLine.add("14");
+        xLine.add("پزشکی");
+        xLine.add("مهندسی پزشکی");
+        xLine.add("englishTitle");
         ArrayList<Entry> yLine=new ArrayList<>();
-        yLine.add(new Entry(15f,0));
-        yLine.add(new Entry(5f,1));
-        yLine.add(new Entry(10f,2));
-        yLine.add(new Entry(30f,3));
-        yLine.add(new Entry(21f,4));
-        yLine.add(new Entry(80f,5));
-        yLine.add(new Entry(5f,6));
-        yLine.add(new Entry(15f,7));
-        yLine.add(new Entry(20f,8));
-        yLine.add(new Entry(0f,9));
-        yLine.add(new Entry(15f,10));
-        yLine.add(new Entry(10f,11));
-        yLine.add(new Entry(21f,12));
-        yLine.add(new Entry(25f,13));
-        yLine.add(new Entry(7f,14));
+        yLine.add(new Entry(20,0));
+        yLine.add(new Entry(12,1));
+        yLine.add(new Entry(10,2));
         LineDataSet datset=new LineDataSet(yLine,"LineChart");
         datset.setColors(ColorTemplate.COLORFUL_COLORS);
         LineData lineData=new LineData(xLine,datset);
         chart1.setData(lineData);
+        chart1.setDescription("");
 
 
         //PieChart Set Data
         ArrayList<String> x=new ArrayList<>();
-        x.add("data0");
-        x.add("data1");
-        x.add("data2");
-        x.add("data3");
+        x.add("فیزیولوژی");
+        x.add("دهان و دندان");
         ArrayList<Entry> entries = new ArrayList<>();
-        entries.add(new Entry(18.5f,0));
-        entries.add(new Entry(26.7f, 1));
-        entries.add(new Entry(24.0f,2));
-        entries.add(new Entry(30.8f,3));
+        entries.add(new Entry(65,0));
+        entries.add(new Entry(35, 1));
         PieDataSet set = new PieDataSet(entries,"Election Results");
         set.setColors(ColorTemplate.COLORFUL_COLORS);
         PieData data = new PieData(x,set);
         chart2.setData(data);
-        chart3.setData(data);
+        chart2.setCenterText(getString(R.string.sub_group));
+        chart2.setDrawXValues(false);
+        chart2.setDescription("");
+
+
+        ArrayList<String> x1=new ArrayList<>();
+        x1.add("زبان");
+        x1.add("دندان جلو");
+        ArrayList<Entry> entries1 = new ArrayList<>();
+        entries1.add(new Entry(85,0));
+        entries1.add(new Entry(15, 1));
+        PieDataSet set1 = new PieDataSet(entries1,"Election Results");
+        set1.setColors(ColorTemplate.PASTEL_COLORS);
+        PieData data1 = new PieData(x1,set1);
+        chart3.setData(data1);
+        chart3.setCenterText(getString(R.string.course));
+        chart3.setDrawXValues(false);
+        chart3.setDescription("");
 
     }
 
