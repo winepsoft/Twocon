344e53f267d8474f870c3d81cf3adb77dbeddb0f
shaisteS <shs.woem@gmail.com>
2016-10-31 13:58:23 +0330

    set color picker to setting page

--- app/src/main/java/winep/ir/twocon/Presenter/SettingsPage/SettingsActivity.java
+++ app/src/main/java/winep/ir/twocon/Presenter/SettingsPage/SettingsActivity.java
@@ -1,10 +1,18 @@
 package winep.ir.twocon.Presenter.SettingsPage;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
+import com.getbase.floatingactionbutton.FloatingActionButton;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 import winep.ir.twocon.R;
@@ -14,6 +22,10 @@ import winep.ir.twocon.Utility.Utilities;
  * Created by ShaisteS on 10/15/2016.
  */
 public class SettingsActivity extends AppCompatActivity {
+
+    private Context context;
+    private FloatingActionButton btnSelectColor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +33,43 @@ public class SettingsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle(getString(R.string.settings_activity_title));
+        context=this;
+        btnSelectColor=(FloatingActionButton)findViewById(R.id.btn_settings_app_color);
+        btnSelectColor.setSize(com.getbase.floatingactionbutton.FloatingActionButton.SIZE_MINI);
+        btnSelectColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ColorPickerDialogBuilder
+                        .with(context)
+                        .setTitle(R.string.color_picker_dialog_title)
+                        .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                        .showAlphaSlider(false)
+                        .density(10)
+                        .setOnColorSelectedListener(new OnColorSelectedListener() {
+                            @Override
+                            public void onColorSelected(int selectedColor) {
+                            }
+                        })
+                        .setPositiveButton(R.string.save, new ColorPickerClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                                btnSelectColor.setColorNormal(selectedColor);
+                                btnSelectColor.setColorPressed(selectedColor);
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .build()
+                        .show();
+
+            }
+        });
+
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
