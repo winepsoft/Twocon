65595cbcbdf8ececbfed62cc16f8ee3306cffdb8
shaisteS <shs.woem@gmail.com>
2016-11-09 15:12:45 +0330

    set action bar for exam page

--- app/src/main/java/winep/ir/twocon/Presenter/ExamPage/ExamActivity.java
+++ app/src/main/java/winep/ir/twocon/Presenter/ExamPage/ExamActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 import winep.ir.twocon.R;
@@ -24,10 +26,40 @@ public class ExamActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.title_back_menu, menu);
+        if(Utilities.getInstance().isRTL()) {
+            menu.getItem(0).setIcon(R.mipmap.back_fa);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        }
+        else {
+            menu.getItem(0).setVisible(false);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id==android.R.id.home)
+            finish();
+        if (id==R.id.action_back)
+            finish();
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
         Utilities.getInstance().setSettingLanguage(newBase);
 
     }
+
 
 }
