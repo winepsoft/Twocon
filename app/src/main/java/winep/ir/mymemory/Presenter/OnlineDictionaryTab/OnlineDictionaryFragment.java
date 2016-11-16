875d585db75919ee6413936480b7118cbc26ef8a
shaisteS <shs.woem@gmail.com>
2016-11-16 17:23:25 +0330

    modify online question tab

--- app/src/main/java/winep/ir/mymemory/Presenter/OnlineDictionaryTab/OnlineDictionaryFragment.java
+++ app/src/main/java/winep/ir/mymemory/Presenter/OnlineDictionaryTab/OnlineDictionaryFragment.java
@@ -1,15 +1,21 @@
 package winep.ir.mymemory.Presenter.OnlineDictionaryTab;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.rey.material.widget.Spinner;
 
+import winep.ir.mymemory.DataModel.Question;
+import winep.ir.mymemory.Presenter.CreateQuestionPage.CreateQuestionActivity;
 import winep.ir.mymemory.R;
 import winep.ir.mymemory.Utility.Utilities;
 
@@ -20,6 +26,9 @@ public class OnlineDictionaryFragment extends Fragment {
 
     private Spinner spinnerFromLanguage;
     private Spinner spinnerToLanguage;
+    private Button btnAddMemoryBank;
+    private EditText editTextTranslateFrom;
+    private TextView txtTranslateTo;
     private Context context;
 
     @Override
@@ -33,6 +42,18 @@ public class OnlineDictionaryFragment extends Fragment {
         a.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerFromLanguage.setAdapter(a);
         spinnerToLanguage.setAdapter(a);
+
+        txtTranslateTo=(TextView)mainView.findViewById(R.id.txt_translation_page_to_text);
+        btnAddMemoryBank=(Button)mainView.findViewById(R.id.btn_translation_page_add_memory_bank);
+        btnAddMemoryBank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Question aQuestion=new Question();
+                Intent intent=new Intent();
+                intent.setClass(context, CreateQuestionActivity.class);
+                context.startActivity(intent);
+            }
+        });
 
 
         return mainView;
