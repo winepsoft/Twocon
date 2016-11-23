d5b6794a31ff4f883e96af1362569a30af307257
shaisteS <shs.woem@gmail.com>
2016-11-23 09:30:02 +0330

    after launch to main page close login page

--- app/src/main/java/winep/ir/mymemory/Presenter/LoginPage/MainActivity.java
+++ app/src/main/java/winep/ir/mymemory/Presenter/LoginPage/MainActivity.java
@@ -53,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent=new Intent(context, MainPage.class);
                 startActivity(intent);
+                finish();
             }
         });
 
